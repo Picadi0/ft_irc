65aadc3699f43833d11de8ba9b1adb9ae948c120
Melih Altun <maltun@k1m22s01.42istanbul.com.tr>
2024-10-18 16:02:17 +0300

    quick fixes

--- src/commands/mode.cpp
+++ src/commands/mode.cpp
@@ -145,9 +145,6 @@ void IRC::modecmd(string targetChannel, string mode, string param, Client &sende
                             }
                         }
                     }
-                    else {
-                        sendMsg(sender.getSockfd(), "401 Error : Unknown " + mode);
-                    }
                 }
             }
             else if (channel == NULL)
